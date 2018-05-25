74312dbcdbb3b03b2a2a5496cd2622a3c5ec6271
Daniel Cruz <Pokedann@MacBook-Pro-de-Daniel.local>
2018-05-25 00:34:14 -0500

    Apocalipsis zombie JS
    Ejercicio para aprender JS y probar algunas funciones de la consola
    como console.warn, Math.random, etc.

---
+++ main.js
@@ -0,0 +1,64 @@
+// alert("que tranza mundo");
+// confirm("como estas?");
+// prompt("estas seguro de que estas seguro?");
+
+console.log("Que tranza Pokedann");
+console.warn("Esto es un asalto a tu corazon");
+console.error("Esto es como mi ex, o sea, es un error");
+
+// var myName = "Pokedann";
+// alert(myName);
+
+// var pokemonFavorito = prompt("Menciona un Pokemon:");
+
+// if (pokemonFavorito === "lucario") {
+// 	alert("El mejor pokemon de todos")
+// }
+// else if (pokemonFavorito === "electivire"){
+// 	alert("Me encantan los pokemon electricos")
+// }
+// else{
+// 	alert("El pokemon que escogiste es: " + pokemonFavorito);
+// }
+
+// var numeroRandom = Math.round(Math.random()*15);
+// console.log(numeroRandom);
+// if (numeroRandom % 3 === 0 && numeroRandom != 0) {
+// 	alert("Concept");
+// }
+// else if (numeroRandom % 5 === 0 && numeroRandom != 0){
+// 	alert("Haus");
+// }
+// else if(numeroRandom % 3 && numeroRandom % 5){
+// 	alert("Concept Haus");
+// }
+
+alert("El apocalipsis zombie acaba de empezar, estas programando en JS muy tranquilo(a) un dia en tu casa y de repente unos zombies tiran la puerta");
+var arma = prompt("Debes buscar un arma a la de ya,que escogerias: flecha y arco, una hacha o un peluche de pikachu?");
+var numeroRandom = Math.round(Math.random());
+
+alert("Has atacado al zombie con: " + arma);
+
+if (numeroRandom === 0) {
+	alert("El zombie te mordio por menso, perdistessss");
+}
+else if (numeroRandom === 1){
+	alert("Matastessss al zombie con: " + arma + "es por eso que ganastessss");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
